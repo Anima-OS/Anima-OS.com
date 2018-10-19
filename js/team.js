b70e526f36329c899de3fb914293b961a59e19fe
Happy-Ferret <MarkBauer@gmx.ch>
2018-10-19 11:51:37 +0200

    Update team.js

--- js/team.js
+++ js/team.js
@@ -54,7 +54,7 @@ angular.module('team', []).controller('teamController', function($scope, $http) 
   //              //change timezone to its abbreviation
                 try {
                     // member.tz = moment().tz(member.tz).format('z');
-                    member.tz = moment(currentdate).tz(member.tz).format('hma z');
+                    member.tz = moment(currentdate).tz(member.tz).format('h:m a z');
                 } catch (err) {
                     console.warn(err);
                 }
